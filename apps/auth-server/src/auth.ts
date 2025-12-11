aff951bec29546f837e65dbb5c25d729c012c38e
Oung Seik <mhemaungthuwin@gmail.com>
2025-12-11 09:04:51 +0630

    feat: setup organization plugin

--- apps/auth-server/src/auth.ts
+++ apps/auth-server/src/auth.ts
@@ -1,15 +1,20 @@
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
-import { jwt } from "better-auth/plugins";
+import { jwt, organization as organizationPlugin } from "better-auth/plugins";
 import { drizzle } from "drizzle-orm/tursodatabase/database";
-import { session, user } from "./schema/core";
+import { account, session, user, verification } from "./schema/core";
+import { invitation, member, organization } from "./schema/organization";
+import { relations } from "./schema/relations";
 
-const db = drizzle(process.env.AUTH_DATABASE_URL!, { schema: { user, session } });
+const db = drizzle(process.env.AUTH_DATABASE_URL!, {
+  schema: { user, session, account, verification, invitation, member, organization },
+  relations,
+});
 
 export const auth = betterAuth({
   database: drizzleAdapter(db, { provider: "sqlite" }),
   experimental: { joins: true },
 
   emailAndPassword: { enabled: true, requireEmailVerification: true },
-  plugins: [jwt()],
+  plugins: [jwt(), organizationPlugin()],
 });
